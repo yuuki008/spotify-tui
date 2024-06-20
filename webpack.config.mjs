6d942d0ae0b04d48c92c113e8cbcf3f5fc814b5b
yuuki008 <s22ah261@gmail.com>
2024-06-20 13:47:16 +0900

    トランスパイルとビルド設定

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,7 +1,6 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import webpackNodeExternals from 'webpack-node-externals';
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -9,22 +8,52 @@ export default {
   mode: 'development',
   entry: './src/index.tsx',
   target: 'node',
-  externals: [webpackNodeExternals()],
+  externals: [webpackNodeExternals({
+    allowlist: (moduleName) => {
+      // NOTE: keytar ライブラリの中身はバイナリファイルでトランスパイル時にエラーが発生するため除外
+      if (moduleName === 'keytar') return false;
+
+      // NOTE: Expressはテンプレートエンジン機能があり、動的に読み込むファイルが変更されるため静的なトランスパイルができず、警告が出る。
+      // express は webpack でビルドを行わないようにすることで警告を表示しないようにしている
+      // 詳細については以下を参照:
+      // https://github.com/webpack/webpack/issues/1576
+      if (moduleName === 'express') return false;
+
+      return true;
+    }
+  })],
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /\.(ts|tsx)$/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.node$/,
+        use: 'node-loader',
+      }
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
   },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-};
-
+}
